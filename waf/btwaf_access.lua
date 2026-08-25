c006f149025ef007de2e054b575c0336d266f315
tianquege <wangn5635@gmail.com>
2026-08-26 07:23:17 +0800

    fix: whitelist custom security webhook path

--- waf/btwaf_access.lua
+++ waf/btwaf_access.lua
@@ -137,8 +137,8 @@ end
 -- 1. 框架专属防御
 if site_framework == "v2board" then
     -- V2Board 专属放行逻辑（API 订阅与服务端节点通信免死金牌）
-    -- 兼容默认订阅路径，以及用户自定义的安全订阅路径，以及 Telegram 官方 Bot Webhook
-    if string.find(req_uri, "/api/v1/client/subscribe") or string.find(req_uri, "/api/v1/server/") or string.find(req_uri, "/ktelie/verxcen/cliuekub/siktdlext") or string.find(req_uri, "/telegram/webhook") then
+    -- 兼容默认订阅路径，以及用户自定义的安全订阅路径，以及 Telegram 官方 Bot Webhook 和第三方安全防控机器人的回调
+    if string.find(req_uri, "/api/v1/client/subscribe") or string.find(req_uri, "/api/v1/server/") or string.find(req_uri, "/ktelie/verxcen/cliuekub/siktdlext") or string.find(req_uri, "/telegram/webhook") or string.find(req_uri, "/security/webhook") then
         return
     end
 
